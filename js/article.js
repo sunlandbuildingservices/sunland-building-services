656fd64460ae95a197e5e2476917054f4727789c
captainspaceman <ryan@cartoonfood.com>
2017-05-09 15:46:10 -0700

    fixes slide toggle on article pages and adds content to the homepage

--- js/article.js
+++ js/article.js
@@ -2,7 +2,7 @@
 //need to create a function that works with the "a"'s  to show and hide them. 
 $(document).ready(function() { 
 	console.log("File has loaded");
- $("a").click(function(e){
+ $(".read-more").click(function(e){
  	e.preventDefault(); 
  	console.log("click happened and default prevented");
  //prevents the "jump" of the link click
@@ -15,16 +15,17 @@ $('.read-more').click(function(){
 	$('#show-this-on-click').slideDown();
 	$('.read-more').hide();
 	$('.read-less').show();
-});
+})
 	
-});
 
-$('.read-less').click(function(){
-	console.log("read less button clicked");
-	$('.read-less').hide();
+// $('.read-less').click(function(){
+// 	console.log("read less button clicked");
+// 	$('.read-less').hide();
+
+// })
 	$('#show-this-on-click').slideUp(function(){
 		$('.read-more').show();
 	});
+
+
 });
-
-
