5fa5fca6ddae449e53e18b223760aaf70d507d96
Ryan Farrell <ryan@cartoonfood.com>
2018-10-05 02:09:06 -0700

    Adds script.js and Firebase code to the web app

---
+++ public/js/script.js
@@ -0,0 +1,11 @@
+//Firebase Hosting 
+// Initialize Firebase
+  var config = {
+    apiKey: "AIzaSyDxJLZyK2jXy3vzkjFvJkefu7sm9Ryv9fw",
+    authDomain: "sunland-building-services.firebaseapp.com",
+    databaseURL: "https://sunland-building-services.firebaseio.com",
+    projectId: "sunland-building-services",
+    storageBucket: "sunland-building-services.appspot.com",
+    messagingSenderId: "923772014824"
+  };
+  firebase.initializeApp(config);
